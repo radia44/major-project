272c6a47fb8a4f09f0cde52c6141346d22eb576a
radia44 <157192250+radia44@users.noreply.github.com>
2024-11-26 10:23:23 -0600

    Made proposal and grid function

--- sketch.js
+++ sketch.js
@@ -15,3 +15,11 @@ function draw() {
 
   circle(mouseX, mouseY, 100);
 }
+
+function displayGrid() {
+  for (let y = 0; y < GRID_SIZE; y++) {
+    for (let x = 0; x < GRID_SIZE; x++) {
+      square(x * cellSize, y * cellSize, cellSize);
+    }
+  }
+}
