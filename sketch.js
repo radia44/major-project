4ae4b79dc51d49f09ae8f6d0d3a6524f6e5eb846
Jannat1302225 <1302225@spsd.sk.ca>
2024-12-10 10:21:58 -0600

    Added input area to sidebar

--- sketch.js
+++ sketch.js
@@ -10,17 +10,31 @@ let graphingButton;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  createStartScreen(); // Draw startscreen
 }
 
 function draw() {
   if (state === "start") {
     drawStartScreen();
-    drawGraphingButton();
-    graphingButton.mouseClicked(stateNowGraphing);
   }
-  if (state === "graphing"){
+  else if (state === "graphing") {
     drawGraphingScreen();
+    drawSidebar();
   }
+}
+
+function createStartScreen() {
+  //Draw "Graphing" button
+  graphingButton = createButton("Graphing");
+  graphingButton.size(120, 40);
+  graphingButton.position(width / 2 - 60, height / 2 - 20); 
+  graphingButton.style("font-family", "sans-serif");
+  graphingButton.style("font-size", "16px");
+  graphingButton.style("background-color", "blue");
+  graphingButton.style("color", "white");
+  graphingButton.style("border", "none");
+  graphingButton.style("border-radius", "5px");
+  graphingButton.mousePressed(stateNowGraphing); // Attach event listener function
 }
 
 function drawStartScreen() {
@@ -28,28 +42,41 @@ function drawStartScreen() {
   textAlign(CENTER, CENTER);
   fill("white");
   textSize(30);
-  text("Function Graphing Calculator", width/2, height/2 - 100);
+  text("Function Graphing Calculator", width / 2, height / 2 - 100);
   textSize(18);
-  text("Options", width/2, height/2 - 50);
-}
-
-function drawGraphingButton() {
-  graphingButton = createButton("Graphing");
-  graphingButton.size(120, 40);
-  graphingButton.position(width/2, height/2);
-  graphingButton.style("font-family", "sans-serif");
-  graphingButton.style("font-size", "12px");
-  graphingButton.color("blue");
+  text("Options", width / 2, height / 2 - 50);
 }
 
 function stateNowGraphing() {
   state = "graphing";
+  graphingButton.hide(); // Hide the button when state is graphing
 }
 
 function drawGraphingScreen() {
   background(255);
 
+  // Draw Cartesian axes
   stroke("black");
-  line(width/2, 0, width/2, height);
-  line(0, height/2, width, height/2);
+  // X-axis starts after the sidebar
+  line(210, height / 2, width, height / 2); 
+  // Y-axis
+  line((width + 210) / 2, 0, (width + 210) / 2, height); 
+}
+
+function drawSidebar() {
+  // Draw the sidebar background
+  fill(240);
+  noStroke();
+  rect(0, 0, 210, height);
+
+  // Add sidebar title
+  fill(0);
+  textSize(16);
+  textAlign(LEFT, TOP);
+  textStyle("bold");
+  text("Function Input", 20, 20); 
+
+  // Add input
+  input = createInput('');
+  input.position(20, 50); 
 }
